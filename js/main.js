be9a0633beebfc430e7a97f39b29d66451d330f6
Tarps240 <sl3966@gmail.com>
2024-10-01 16:50:35 -0700

    Final Project v1.0 commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const menu = document.querySelector('.menu');
+
+mobileMenu.addEventListener('click', () => {
+    menu.classList.toggle('open');
+    console.log(menu.classList);
+});
